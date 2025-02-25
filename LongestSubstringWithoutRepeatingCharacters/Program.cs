7c4c2938ad06e5881c1411505783086b24f66ca3
sharofsoliyev1@gmail.com <sharofsoliyev1@gmail.com>
2025-02-25 14:07:44 +0500

    Longest Substring Without Repeating Characters

---
+++ LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+Solution Solution = new Solution();
+
+
+Solution.LengthOfLongestSubstring("qrsvbspk");
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        if (s.Length == 0) return 0;
+        if (s.Length == 1) return 1;
+
+        int maxLenght = 1;
+
+        char[] chars = new char[1];
+        chars[0] = s[0];
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (chars.Contains(s[i]))
+            {
+                char[] newChars = new char[chars.Length - 1];
+                chars = chars.Skip(1).ToArray();
+                if (!chars.Contains(s[i]))
+                {
+                    Array.Resize(ref chars, chars.Length + 1);
+                    chars[chars.Length - 1] = s[i];
+                }
+                else
+                {
+                    int index = Array.IndexOf(chars, s[i]);
+                    chars = chars.Skip(index+1).ToArray();
+
+                    Array.Resize(ref chars, chars.Length + 1);
+                    chars[chars.Length - 1] = s[i];
+
+                }
+
+            }
+            else
+            {
+                Array.Resize(ref chars, chars.Length + 1);
+                chars[chars.Length - 1] = s[i];
+                if (maxLenght < chars.Length) maxLenght = chars.Length;
+            }
+
+        }
+
+        return maxLenght;
+    }
+
+
+}
