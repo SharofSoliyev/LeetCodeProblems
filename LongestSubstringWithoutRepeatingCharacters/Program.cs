e2989b4c49c283a6c0c537c5c21c91b49a9a19c0
sharofsoliyev1@gmail.com <sharofsoliyev1@gmail.com>
2025-02-26 16:37:23 +0500

    [EASY] IS PALINDROME
    https://leetcode.com/problems/palindrome-number/description/

--- LongestSubstringWithoutRepeatingCharacters/Program.cs
+++ LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -4,39 +4,3 @@ Solution Solution = new Solution();
 
 
 Solution.LengthOfLongestSubstring("qrsvbspk");
-public class Solution
-{
-    public int LengthOfLongestSubstring(string s)
-    {
-        if (s.Length == 0) return 0;
-        if (s.Length == 1) return 1;
-
-        int maxLenght = 1;
-
-        char[] chars = new char[1];
-        chars[0] = s[0];
-
-        for (int i = 1; i < s.Length; i++)
-        {
-            if (chars.Contains(s[i]))
-            {
-                int index = Array.IndexOf(chars, s[i]);
-                chars = chars.Skip(index + 1).ToArray();
-
-                Array.Resize(ref chars, chars.Length + 1);
-                chars[chars.Length - 1] = s[i];
-            }
-            else
-            {
-                Array.Resize(ref chars, chars.Length + 1);
-                chars[chars.Length - 1] = s[i];
-                if (maxLenght < chars.Length) maxLenght = chars.Length;
-            }
-
-        }
-
-        return maxLenght;
-    }
-
-
-}
---
+++ PalindromeNumber.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0) return false;
+        if (Reverse(x) == x) return true;
+
+        else return false;
+    }
+
+    public int Reverse(int x)
+    {
+        int reversed = 0;
+        int tempX = x;
+        while (tempX != 0)
+        {
+            int temp = tempX % 10;
+
+            reversed = reversed * 10 + temp;
+
+            tempX = tempX / 10;
+        }
+        return reversed;
+    }
+
+}
